039f8d292bf500be7600e1ba1fd379ae0f44a372
nwduncan <nduncan.au@gmail.com>
2017-01-24 10:31:48 +1100

    Mode class file added.

---
+++ shotModes.py
@@ -0,0 +1,10 @@
+# Bulb ramping mode
+# Investigate the point at which the shutter should be changed from a default
+# shutter speed to bulb mode. ISO ramping also needs to be achievable.
+class Bramp(object):
+    def __init__(self, )
+
+# Steady shot mode
+
+# HDR mode - compatible with other modes?
+# maybe a subset option of shutter mode steady shot?
