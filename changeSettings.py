2f005ddccf06b33b67c1d1a811c37139b06d0500
nwduncan <nduncan.au@gmail.com>
2017-01-24 16:12:42 +1100

    .gitignore updated

--- changeSettings.py
+++ changeSettings.py
@@ -1,3 +1,3 @@
 # The functions in here are called when a change in camera settings is desired.
-# For example when transitioning from present shutter speed to bulb ramping
-# the camera will need to have its settings modified.
+# For example when transitioning from preset shutter speed to bulb ramping the 
+# camera will need to have its settings modified.
