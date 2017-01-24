983c54c3b333395c42ad80ad657382533f63f549
nwduncan <nduncan.au@gmail.com>
2017-01-24 16:06:29 +1100

    EOD update

--- shotModes.py
+++ shotModes.py
@@ -2,7 +2,34 @@
 # Investigate the point at which the shutter should be changed from a default
 # shutter speed to bulb mode. ISO ramping also needs to be achievable.
 class Bramp(object):
-    def __init__(self, )
+    def __init__(self, exln_start, exln_end, interval, bramp_length):
+        if exln_end > interval:
+            raise ValueError("Maximum exposure time cannot exceed shot interval time")
+        self.exln_start = exln_start
+        self.exln_end = exln_end
+        self.interval = interval
+        self.bramp_length = bramp_length
+        self.shotObj = None
+        self.buildShots()
+
+    def buildShots(self):
+        # round total number of shots
+        tot, rem = divmod(self.bramp_length, self.interval)
+        frames = int(tot) if rem <= self.interval/2 else int(tot) + 1
+        # increment to add to each shot
+        ramp_int = (float(self.exln_end) - self.exln_start) / (frames - 1)
+        # list of ramp intervals
+        exposures = [self.exln_start] + [round(self.exln_start + (n * ramp_int), 2) for n in range(1, frames)]
+        self.shotObj = exposures
+
+    def returnShotObj(self):
+        self.buildShots()
+        return self.shotObj
+
+    def __str__(self):
+        return ' | '.join([ "Shot"+str(i+1)+": "+str(self.interval)+"s @"+str(e)+"s" for i,e in enumerate(self.shotObj) ])
+
+
 
 # Steady shot mode
 
