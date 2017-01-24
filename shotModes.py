8d18b88689968ea03a096c77c30ca872d51c5378
nwduncan <nduncan.au@gmail.com>
2017-01-24 16:57:35 +1100

    EOD commit.

--- shotModes.py
+++ shotModes.py
@@ -3,16 +3,20 @@
 # shutter speed to bulb mode. ISO ramping also needs to be achievable.
 class Bramp(object):
     def __init__(self, exln_start, exln_end, interval, bramp_length):
-        if exln_end > interval:
-            raise ValueError("Maximum exposure time cannot exceed shot interval time")
         self.exln_start = exln_start
         self.exln_end = exln_end
+        # the interval variable should probably be part of a mode container class as it is not likely
+        # to change throughout the timelapse
         self.interval = interval
         self.bramp_length = bramp_length
         self.shotObj = None
         self.buildShots()
 
     def buildShots(self):
+        # make sure we're building a valid shot list
+        if self.exln_end > self.interval:
+            # this could be overcome with automatic ISO changes (a limit must be set though)
+            raise ValueError("Maximum exposure time cannot exceed shot interval time.")
         # round total number of shots
         tot, rem = divmod(self.bramp_length, self.interval)
         frames = int(tot) if rem <= self.interval/2 else int(tot) + 1
@@ -27,7 +31,8 @@ class Bramp(object):
         return self.shotObj
 
     def __str__(self):
-        return ' | '.join([ "Shot"+str(i+1)+": "+str(self.interval)+"s @"+str(e)+"s" for i,e in enumerate(self.shotObj) ])
+        # replace with something less shit
+        return ' | '.join([ "Shot"+str(i+1)+": +"+str(self.interval*i)+"s @"+str(e)+"s" for i,e in enumerate(self.shotObj) ])
 
 
 
