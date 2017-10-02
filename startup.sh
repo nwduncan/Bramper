1e70d931987b6f2498e254d14d6b2642dbc21575
William Friesen <wfriesen@gmail.com>
2017-10-02 11:44:25 +1100

    use pushd and popd in startup script

--- startup.sh
+++ startup.sh
@@ -2,7 +2,6 @@
 # startup.sh
 # navigate to the home dir, then to this dir, run startup python script, then navigate back to the home dir
 
-cd /
-cd home/pi/Desktop/Bramper
+pushd /home/pi/Desktop/Bramper
 sudo python startup.py
-cd /
+popd
