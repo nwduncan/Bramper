ffd5a31801252ef72d5ae6d3dd5ebd09a6010ae3
Nathan Duncan <nduncan.au@gmail.com>
2017-11-14 16:57:26 +1100

    Refining control scheme and data structure

--- controller.py
+++ controller.py
@@ -13,14 +13,42 @@ GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # These variables serve two purposes:
 # - Calling gphoto2/camera specific config settings
 # - To define where to display these elements in the interface.Display class
+# - Has the potential to allow profiles for different cameras to be set up
 shutter = "shutterspeed2"
 iso = "iso"
 aperture = "f-number"
 misc = "misc"
 
-# initialise the LCD for use
-#           LCD section:   1      2       3       4
-lcd = interface.Display(shutter, iso, aperture, misc)
+# control = { 0: shutter,
+#             1: iso,
+#             2: aperture,
+#             3: misc }
+
+control = { 0: [shutter,
+                  camera_settings.get_config(shutter),
+                  camera_settings.get_options(shutter)]
+              1: [iso,
+                  camera_settings.get_config(iso),
+                  camera_settings.get_options(iso)]
+              2: [aperture,
+                  camera_settings.get_config(shutter),
+                  camera_settings.get_options(shutter)]
+              3:  misc }
+
+
+# initialise the LCD and set the default cursor position
+lcd = interface.Display(control[0][1], control[1][1], control[2][1], control[3][1])
+pos = 0
+lcd.set_cursor_pos(0)
+
+def display_refresh(lcd):
+    lcd.messages[shutter][1] = shutter_current
+    lcd.messages[iso][1] = iso_current
+    lcd.messages[aperture][1] = aperture_current
+    lcd.messages[misc][1] = ""
+    lcd.refresh()
+
+display_refresh(lcd)
 
 # find the index of an item in a list
 def get_idx(item, options):
@@ -29,30 +57,22 @@ def get_idx(item, options):
             return idx
     return False
 
-# shutter
-shutter_current = camera_settings.get_config(shutter)
-shutter_options = camera_settings.get_options(shutter)
-shutter_index = get_idx(shutter_current, shutter_options)
+# # shutter
+# shutter_current = camera_settings.get_config(shutter)
+# shutter_options = camera_settings.get_options(shutter)
+# shutter_index = get_idx(shutter_current, shutter_options)
+#
+# # iso
+# iso_current = camera_settings.get_config(iso)
+# iso_options = camera_settings.get_options(iso)
+# iso_index = get_idx(iso_current, iso_options)
+#
+# # aperture
+# aperture_current = camera_settings.get_config(aperture)
+# aperture_options = camera_settings.get_options(aperture)
+# aperture_index = get_idx(aperture_current, aperture_options)
 
-# iso
-iso_current = camera_settings.get_config(iso)
-iso_options = camera_settings.get_options(iso)
-iso_index = get_idx(iso_current, iso_options)
 
-# aperture
-aperture_current = camera_settings.get_config(aperture)
-aperture_options = camera_settings.get_options(aperture)
-aperture_index = get_idx(aperture_current, aperture_options)
-
-
-# get camera settings and display them
-# this should also update the lists of options and the index of the current setting
-def display_refresh(lcd):
-    lcd.messages[shutter][1] = shutter_current
-    lcd.messages[iso][1] = iso_current
-    lcd.messages[aperture][1] = aperture_current
-    lcd.messages[misc][1] = ""
-    lcd.refresh()
 
 # set initial display
 display_refresh(lcd)
@@ -63,8 +83,8 @@ display_refresh(lcd)
 # cursor movement
 # 0 2
 # 1 3
-pos = 0
-lcd.set_cursor_pos(0)
+
+
 
 # control scheme 2
 # reverse scroll through screen positions
