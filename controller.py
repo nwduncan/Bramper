62bde6e2d53c0eeaf86603cd1615b018d82dfb2a
Nathan Duncan <nduncan.au@gmail.com>
2017-11-15 16:10:48 +1100

    Refinement of control scheme to allow cycling through of config options.

--- controller.py
+++ controller.py
@@ -4,86 +4,70 @@ import RPi.GPIO as GPIO
 import time
 
 
+
 ## Initialise stuff
 # button variables
 button1_pin = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+
 # These variables serve two purposes:
 # - Calling gphoto2/camera specific config settings
 # - To define where to display these elements in the interface.Display class
 # - Has the potential to allow profiles for different cameras to be set up
+# - Currently 'info' represents the section which will display control options (enter, cancel, refresh, etc)
 shutter = "shutterspeed2"
 iso = "iso"
 aperture = "f-number"
-misc = "misc"
+info = "info"
+order = [shutter, iso, aperture, info]
 
-# control = { 0: shutter,
-#             1: iso,
-#             2: aperture,
-#             3: misc }
-
-control = { 0: [shutter,
-                  camera_settings.get_config(shutter),
-                  camera_settings.get_options(shutter)]
-              1: [iso,
-                  camera_settings.get_config(iso),
-                  camera_settings.get_options(iso)]
-              2: [aperture,
-                  camera_settings.get_config(shutter),
-                  camera_settings.get_options(shutter)]
-              3:  misc }
-
+# control = { config_name: [current_setting, options, new_setting] }
+control = { shutter: [camera_settings.get_config(shutter), camera_settings.get_options(shutter), None],
+            iso: [camera_settings.get_config(iso), camera_settings.get_options(iso), None],
+            aperture: [camera_settings.get_config(shutter), camera_settings.get_options(shutter), None]}
 
 # initialise the LCD and set the default cursor position
-lcd = interface.Display(control[0][1], control[1][1], control[2][1], control[3][1])
+lcd = interface.Display(order[0], order[1], order[2], order[3])
 pos = 0
 lcd.set_cursor_pos(0)
 
-def display_refresh(lcd):
+# edit_mode = True
+
+# refresh camera data
+def refresh_camera_config():
+    global control
+
+    for config in control:
+        control[config][0] = camera_settings.get_config(config)
+        control[config][1] = camera_settings.get_options(config)
+
+    return
+
+# refresh display
+def refresh_display():
+    global lcd, control
+
+    for config in control:
+        lcd.messages[config][1] = control[config]
     lcd.messages[shutter][1] = shutter_current
     lcd.messages[iso][1] = iso_current
     lcd.messages[aperture][1] = aperture_current
-    lcd.messages[misc][1] = ""
     lcd.refresh()
 
-display_refresh(lcd)
+refresh_display()
+
+
 
 # find the index of an item in a list
+# used to turn pos variable in to config name
 def get_idx(item, options):
     for idx, opt in enumerate(options):
         if opt == item:
             return idx
     return False
-
-# # shutter
-# shutter_current = camera_settings.get_config(shutter)
-# shutter_options = camera_settings.get_options(shutter)
-# shutter_index = get_idx(shutter_current, shutter_options)
-#
-# # iso
-# iso_current = camera_settings.get_config(iso)
-# iso_options = camera_settings.get_options(iso)
-# iso_index = get_idx(iso_current, iso_options)
-#
-# # aperture
-# aperture_current = camera_settings.get_config(aperture)
-# aperture_options = camera_settings.get_options(aperture)
-# aperture_index = get_idx(aperture_current, aperture_options)
-
-
-
-# set initial display
-display_refresh(lcd)
-
-
-
-## Control scheme stuff
-# cursor movement
-# 0 2
-# 1 3
-
 
 
 # control scheme 2
@@ -103,40 +87,45 @@ def down():
 
 # scroll right through current option values
 def right():
+    global pos, order, lcd, control
+
+    current_name = order[pos]
+    current = control[current_name][0]
+    options = control[current_name][1]
+    current_idx = get_idx(current, options)
+
+    if current_idx < (len(options)-1):
+        new_idx = current_idx+1
+        new_config = options[new_idx]
+    else:
+        new_idx = 0
+        new_config = options[new_idx]
+
+    control[current_name][2] = new_config
+    lcd.set_message(current_name, new_config)
+
     return
 
 # scroll left through current option values
 def left():
+    global pos, order, lcd, control
+
+    current_name = order[pos]
+    current = control[current_name][0]
+    options = control[current_name][1]
+    current_idx = get_idx(current, options)
+
+    if current_idx > 0:
+        new_idx = current_idx-1
+        new_config = options[new_idx]
+    else:
+        new_idx = len(options)-1
+        new_config = options[new_idx]
+
+    control[current_name][2] = new_config
+    lcd.set_message(current_name, new_config)
+
     return
-
-
-# # control scheme 1
-# def up():
-#     global pos
-#     pos-= 1 if pos%2 == 0 else 0
-#     lcd.set_cursor_pos(pos)
-#     return
-#
-# def down():
-#     global pos
-#     pos+= 1 if pos%2 == 1 else 0
-#     lcd.set_cursor_pos(pos)
-#     return
-#
-# def right():
-#     global pos
-#     pos+=2 if pos < 3 else 0
-#     lcd.set_cursor_pos(pos)
-#     return
-#
-# def left():
-#     global pos
-#     pos-=2 if pos > 2 else 0
-#     lcd.set_cursor_pos(pos)
-#     return
-#
-#
-
 
 
 ## Interaction stuff
